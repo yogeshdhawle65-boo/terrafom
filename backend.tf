e6e3d52aaefbb862c718a07cde6317171762664a
yogesh dhawle <yogeshdhawle65@gmail.com>
2026-09-16 16:35:53 +0530

    fe

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
 backend "s3" {
-bucket = "practice-bucket-real-infra-terraform "
+bucket = "practice-bucket-real-infra-terraform"
 key = "projects/myapp1/terraform.tfstate"
 region = "us-east-1"
 encrypt = true
